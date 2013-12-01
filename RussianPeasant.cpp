cd7a9e09c550bca039db957ffa486613a6ad30e1
Charles Doty <cdoty@rastersoft.net>
2013-12-01 05:46:09 -0600

    Added Russian Peasant method

---
+++ RussianPeasant.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void multiply(int _iValue1, int _iValue2, int& _iResult);
+
+int main()
+{
+	int	iValue1 = 25;
+	int	iValue2 = 25;
+
+	printf("%d x %d = ", iValue1, iValue2);
+
+	int	iResult;
+
+	multiply(iValue1, iValue2, iResult);
+
+	printf("%d\n", iResult);
+
+	return	0;
+}
+
+void multiply(int _iValue1, int _iValue2, int& _iResult)
+{
+	_iResult	= 0;
+	
+	while (_iValue2 >= 1)
+	{
+		if (_iValue2 & 1)
+		{
+			_iResult	+= _iValue1;
+		}
+
+		_iValue1	<<= 1;
+		_iValue2	>>= 1;
+	}
+}
